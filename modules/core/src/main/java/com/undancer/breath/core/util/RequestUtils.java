35975f2a568a725015576a14cf315ea7d01c98d0
魏涛 <undancer@gmail.com>
2014-04-23 09:34:48 +0800

    增加几个常用方法

--- modules/core/src/main/java/com/undancer/breath/core/util/RequestUtils.java
+++ modules/core/src/main/java/com/undancer/breath/core/util/RequestUtils.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,10 @@ import javax.servlet.http.HttpServletRequest;
  * Created by undancer on 14-4-16.
  */
 public class RequestUtils implements ApplicationContextAware {
+
+    private static final String QUERY_STRING_SEPARATOR = "?";
+    private static final String PARAMETER_SEPARATOR = "&";
+    private static final String NAME_VALUE_SEPARATOR = "=";
 
     private static ApplicationContext applicationContext;
 
@@ -27,6 +32,47 @@ public class RequestUtils implements ApplicationContextAware {
         } catch (ServletRequestBindingException e) {
             return null;
         }
+    }
+
+    public static String getRequestUri() {
+        String requestUri = getAttribute(WebUtils.FORWARD_REQUEST_URI_ATTRIBUTE);
+        if (requestUri != null) {
+            return requestUri;
+        }
+        return getRequest().getRequestURI();
+    }
+
+    public static String getQueryString() {
+        String requestUri = getAttribute(WebUtils.FORWARD_REQUEST_URI_ATTRIBUTE);
+        if (requestUri != null) {
+            return getAttribute(WebUtils.FORWARD_QUERY_STRING_ATTRIBUTE);
+        }
+        return getRequest().getQueryString();
+    }
+
+    public static String getUrl() {
+        return getRequestUri() + (getQueryString() != null ? QUERY_STRING_SEPARATOR + getQueryString() : "");
+    }
+
+    public static boolean isGetRequest() {
+        return "GET".equalsIgnoreCase(getRequest().getMethod());
+    }
+
+
+    public static boolean isPostRequest() {
+        return "POST".equalsIgnoreCase(getRequest().getMethod());
+    }
+
+    public static boolean isAjaxRequest() {
+        return "XMLHttpRequest".equalsIgnoreCase(getHeader("X-Requested-With"));
+    }
+
+    public static String getHeader(String name) {
+        return getRequest().getHeader(name);
+    }
+
+    public static <T> T getAttribute(String name) {
+        return (T) getRequest().getAttribute(name);
     }
 
     public static ServletContext getServletContext() {
