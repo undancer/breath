7ebe656dbc1d783106e28a39fc435c4b4715000c
魏涛 <undancer@gmail.com>
2014-06-06 14:11:15 +0800

    当没有找到模版时返回404状态码

--- modules/core/src/main/java/com/undancer/breath/core/ExceptionResolver.java
+++ modules/core/src/main/java/com/undancer/breath/core/ExceptionResolver.java
@@ -2,8 +2,8 @@ package com.undancer.breath.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 import org.springframework.web.servlet.view.InternalResourceView;
 
 import javax.inject.Named;
@@ -14,14 +14,14 @@ import javax.servlet.http.HttpServletResponse;
  * Created by undancer on 14-4-25.
  */
 @Named
-public class ExceptionResolver implements HandlerExceptionResolver {
+public class ExceptionResolver extends AbstractHandlerExceptionResolver {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(ExceptionResolver.class);
 
-    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
+    protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
 
         if (e instanceof ViewNotFoundException) {
-            //return new ModelAndView("forward:/error");
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return new ModelAndView(new InternalResourceView("/404"));
         }
 
